34c1aeb17d39559d503ddc528b94dbca6a11020a
sil <asml.silence@gmail.com>
2017-05-02 22:28:10 +0300

    Extract face feature extractor class

---
+++ effects/face.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import dlib
+import numpy as np
+
+
+JAW_POINTS = list(range(0, 17))
+FACE_POINTS = list(range(17, 68))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+NOSE_POINTS = list(range(27, 35))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+MOUTH_POINTS = list(range(48, 61))
+
+ALIGN_POINTS = LEFT_BROW_POINTS + RIGHT_EYE_POINTS + \
+                LEFT_EYE_POINTS + RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+
+
+class Detector:
+    PREDICTOR = './data/classifiers/shape_predictor_68_face_landmarks.dat'
+
+    def __init__(self, **kargs):
+        predictor_path = Detector.PREDICTOR
+        if 'predictor_path' in kargs:
+            predictor_path = kargs[predictor_path]
+
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(predictor_path)
+
+    def detect(self, image):
+        rects = self.detector(image, 1)
+
+        faces = []
+        for rect in rects:
+            parts = self.predictor(image, rect).parts()
+            points = np.matrix([[p.x, p.y] for p in parts])
+            faces.append(points)
+
+        return faces
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import cv2
 import numpy as np
 import sys
 
+import effects.face as face
+
 xs = [142, 138, 131, 128, 130, 144, 157, 180, 203, 226, 255, 286, 313, 325, 332, 339, 345, 153, 177, 195, 217, 231, 264, 294, 315, 338, 350, 241, 238, 237, 227, 202, 210, 221, 229, 241, 160, 184, 211, 219, 194, 169, 266, 287, 314, 333, 318, 290, 163, 182, 203, 218, 237, 254, 271, 255, 237, 208, 187, 177, 169, 194, 217, 231, 249, 232, 217, 196]
 ys = [201, 222, 248, 281, 304, 346, 356, 381, 391, 390, 375, 353, 331, 315, 293, 267, 250, 170, 164, 169, 182, 200, 209, 201, 199, 201, 215, 224, 247, 267, 288, 297, 303, 307, 309, 306, 205, 194, 207, 233, 236, 230, 249, 228, 226, 245, 265, 263, 306, 311, 316, 321, 322, 325, 329, 346, 359, 358, 346, 334, 321, 322, 343, 242, 339, 336, 337, 332]
 pts2 = np.zeros((len(xs), 2), dtype=int)
@@ -19,64 +21,11 @@ pts2[:, 1] = np.array(ys)
 
 mermaid_img = cv2.imread('./data/images/mermaid.png', cv2.IMREAD_UNCHANGED)
 
-
 SCALE_FACTOR = 1
 FEATHER_AMOUNT = 11
-
-FACE_POINTS = list(range(17, 68))
-MOUTH_POINTS = list(range(48, 61))
-RIGHT_BROW_POINTS = list(range(17, 22))
-LEFT_BROW_POINTS = list(range(22, 27))
-RIGHT_EYE_POINTS = list(range(36, 42))
-LEFT_EYE_POINTS = list(range(42, 48))
-NOSE_POINTS = list(range(27, 35))
-JAW_POINTS = list(range(0, 17))
-
-# Points used to line up the images.
-ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS + RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
-
-# Points from the second image to overlay on the first. The convex hull of each
-# element will be overlaid.
-OVERLAY_POINTS = [
-    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
-    NOSE_POINTS + MOUTH_POINTS,
-]
-
-# Amount of blur to use during colour correction, as a fraction of the
-# pupillary distance.
 COLOUR_CORRECT_BLUR_FRAC = 0.6
-SHAPE_PREDICTOR = './data/classifiers/shape_predictor_68_face_landmarks.dat'
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
-
 
 # ========================================================
-
-class TooManyFaces(Exception):
-    pass
-
-class NoFaces(Exception):
-    pass
-
-def get_landmarks(im):
-    rects = detector(im, 1)
-
-    if len(rects) != 1:
-        return None
-
-    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
-
-def annotate_landmarks(im, landmarks):
-    im = im.copy()
-    for idx, point in enumerate(landmarks):
-        pos = (point[0, 0], point[0, 1])
-        cv2.putText(im, str(idx), pos,
-                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                    fontScale=0.4,
-                    color=(0, 0, 255))
-        cv2.circle(im, pos, 3, color=(0, 255, 255))
-    return im
 
 def draw_convex_hull(im, points, color):
     points = cv2.convexHull(points)
@@ -85,7 +34,7 @@ def draw_convex_hull(im, points, color):
 def get_face_mask(im, landmarks):
     im = np.zeros(im.shape[:2], dtype=np.float64)
 
-    for group in OVERLAY_POINTS:
+    for group in face.OVERLAY_POINTS:
         draw_convex_hull(im,
                          landmarks[group],
                          color=1)
@@ -107,8 +56,6 @@ def transformation_from_points(points1, points2):
     # standard deviation, and then using the SVD to calculate the rotation. See
     # the following for more details:
     #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
-
-    print(points1.shape, points2.shape)
 
     points1 = points1.astype(np.float64)
     points2 = points2.astype(np.float64)
@@ -149,29 +96,12 @@ def warp_im(im, M, dshape):
                    flags=cv2.WARP_INVERSE_MAP)
     return output_im
 
-def correct_colours(im1, im2, landmarks1):
-    blur_amount = COLOUR_CORRECT_BLUR_FRAC * np.linalg.norm(
-                              np.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
-                              np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
-    blur_amount = int(blur_amount)
-    if blur_amount % 2 == 0:
-        blur_amount += 1
-    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
-    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
-
-    # Avoid divide-by-zero errors.
-    im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
-
-    return (im2.astype(np.float64) * im1_blur.astype(np.float64) /
-                                                im2_blur.astype(np.float64))
-
 # ========================================================
 
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
 print('[INFO] loading facial landmark predictor...')
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
+face_detector = face.Detector()
 
 # initialize the video stream and allow the cammera sensor to warmup
 print('[INFO] camera sensor warming up...')
@@ -179,47 +109,26 @@ vs = VideoStream().start()
 time.sleep(2.0)
 
 
-def read_im_and_landmarks(fname):
-    im = cv2.imread(fname, cv2.IMREAD_COLOR)
-    im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR,
-                         im.shape[0] * SCALE_FACTOR))
-    s = get_landmarks(im)
-    im = im / 255.0
-
-    return im, s
-
-
 while True:
 
     im1 = vs.read()
     im1 = imutils.resize(im1, width=400)
     im1 = cv2.resize(im1, (im1.shape[1], im1.shape[0]))
-    landmarks1 = get_landmarks(im1)
+    landmarks1 = face_detector.detect(im1)
     im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2BGRA)
 
     im2 = mermaid_img
     im2 = cv2.resize(im2, (im2.shape[1], im2.shape[0]))
     landmarks2 = pts2[:]
 
-    # print(landmarks1.shape, landmarks2.shape)
-
-    # im1, landmarks1 = im1s, landmarks1s
-    # im2, landmarks2 = im2s, landmarks2s
-
     output_im = None
 
-    if landmarks1 is not None:
-        M = transformation_from_points(landmarks1[ALIGN_POINTS],
-                                       landmarks2[ALIGN_POINTS])
+    if len(landmarks1) >= 1:
+        landmarks1 = landmarks1[0]
+        M = transformation_from_points(landmarks1[face.ALIGN_POINTS],
+                                       landmarks2[face.ALIGN_POINTS])
 
         mask = get_face_mask(im2, landmarks2)
-        # warped_mask = warp_im(mask, M, im1.shape)
-
-
-
-        # combined_mask = np.max([get_face_mask(im1, landmarks1), warped_mask], axis=0)
-        # print(combined_mask)
-
         warped_im2 = warp_im(im2, M, im1.shape)
         alpha = (warped_im2[:, :, 0] == 0) * 255
         beta = 255 - alpha
@@ -228,11 +137,7 @@ while True:
             im1[:, :, i] = np.bitwise_and(im1[:, :, i], alpha)
             warped_im2[:, :, i] = np.bitwise_and(warped_im2[:, :, i], beta)
 
-        # warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
-
-        # print(1)
         output_im = warped_im2 + im1
-        # output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
     else:
         output_im = im1
 
